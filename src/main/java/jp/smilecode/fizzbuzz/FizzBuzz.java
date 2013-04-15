816209a230758332fb7b663878dfe4721833661d
tmaeda1981jp <tmaeda1981jp@gmail.com>
2013-04-15 22:31:53 +0900

    Fix method name and tests

--- src/main/java/jp/smilecode/fizzbuzz/FizzBuzz.java
+++ src/main/java/jp/smilecode/fizzbuzz/FizzBuzz.java
@@ -1,24 +1,21 @@
 package jp.smilecode.fizzbuzz;
 
-public class FizzBuzz {
+class FizzBuzz {
 
-    String run(int num) {
-        if (num%3 == 0 && num%5 == 0) {
-            return "FizzBuzz";
+    String say(int num) throws IllegalArgumentException {
+        if (num < 1) {
+            throw new IllegalArgumentException("1以上の数を指定してください．");
         }
-        if (num%3 == 0) {
-            return "Fizz";
-        }
-        if (num%5 == 0) {
-            return "Buzz";
-        }
+        if (num%3 == 0 && num%5 == 0) return "FizzBuzz";
+        if (num%3 == 0) return "Fizz";
+        if (num%5 == 0) return "Buzz";
         return String.valueOf(num);
     }
     
     public static void main(String[] args) {
         FizzBuzz fizzBuzz = new FizzBuzz();
         for (int i=1; i<=100; i++) {
-            System.out.println(fizzBuzz.run(i));
+            System.out.println(fizzBuzz.say(i));
         }
     }
 }
--- src/test/java/jp/smilecode/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/jp/smilecode/fizzbuzz/FizzBuzzTest.java
@@ -10,15 +10,30 @@ import org.junit.runner.*;
 @RunWith(Enclosed.class)
 public class FizzBuzzTest {
     public static class runメソッドのテスト {
-        private FizzBuzz instance = new FizzBuzz();
-        @Test public void runに3を渡すとFizzを返すこと() {
-            assertThat(instance.run(3), is("Fizz"));
+        private FizzBuzz fizzbuzz = new FizzBuzz();
+
+        @Test public void runに3の倍数且つ5の倍数となる数値を渡すとFizzBuzzという文字列を返すこと() {
+            assertThat(fizzbuzz.say(15), is("FizzBuzz"));
         }
-        @Test public void runに5を渡すとBuzzを返すこと() {
-            assertThat(instance.run(5), is("Buzz"));
+
+        @Test public void runに3の倍数の数値を渡すとFizzという文字列を返すこと() {
+            assertThat(fizzbuzz.say(3), is("Fizz"));
+            assertThat(fizzbuzz.say(126), is("Fizz"));
+            assertThat(fizzbuzz.say(333), is("Fizz"));
         }
-        @Test public void runに15を渡すとFizzBuzzを返すこと() {
-            assertThat(instance.run(15), is("FizzBuzz"));
+        @Test public void runに5の倍数の数値を渡すとBuzzという文字列を返すこと() {
+            assertThat(fizzbuzz.say(5), is("Buzz"));
+            assertThat(fizzbuzz.say(50), is("Buzz"));
+            assertThat(fizzbuzz.say(500), is("Buzz"));
+        }
+        @Test public void runに3または5の倍数ではない数値を渡すと渡した数値を文字列として返すこと() {
+            assertThat(fizzbuzz.say(4), is(String.valueOf("4")));
+            assertThat(fizzbuzz.say(16), is(String.valueOf("16")));
+            assertThat(fizzbuzz.say(34), is(String.valueOf("34")));
+        }
+        @Test(expected=IllegalArgumentException.class)
+        public void runに1より小さい数値を渡すとIllegalArgumentsExceptionをthrowすること() {
+            fizzbuzz.say(0);
         }
     }
 }
